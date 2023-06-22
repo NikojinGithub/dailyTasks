c521b00a0c9a07828149616b7f6f1ee52a648cb9
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2023-06-22 21:11:48 +0400

    add: select day for add task

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,25 @@ const days = document.querySelectorAll('.day__tasks');
 const inputForm = document.querySelector('.form__input');
 const buttonConfirm = document.querySelector('.form__button');
 const form = document.querySelector('.form');
+const daysWeek = document.querySelectorAll('.day');
+const dayNames = document.querySelectorAll('.day__title');
+
+//Select day for task
+function selectDay(set){
+  set.forEach(day => {
+    day.addEventListener('click', (evt) => {
+      if(evt.target.closest('.day__title')){
+        const checkbox = day.querySelector('.day__input');
+        checkbox.checked = !checkbox.checked;
+        day.classList.toggle('day_active')
+      }
+    })
+  })
+}
+
+selectDay(daysWeek);
+
+
 
 
 //Create task
@@ -9,15 +28,21 @@ function createTask(){
   const task = document.querySelector('#template').content.querySelector('.task').cloneNode(true);
   const taskName = task.querySelector('.task__title');
   taskName.textContent = inputForm.value;
-  clearInput(inputForm);
   copyTask(task);
   return task;
 }
 
 //Add task on page
-function addTaskToPage(evt){
+function addTask(evt, days){
   evt.preventDefault();
-  days[0].append(createTask());
+  days.forEach(day => {
+    const checkbox = day.querySelector('input');
+    if(checkbox.checked){
+      const task = day.querySelector('.day__tasks');
+      task.append(createTask())
+    }
+  })
+  clearInput(inputForm);
 }
 
 //Clear input
@@ -33,43 +58,62 @@ function copyTask(elem){
   })
 }
 
+//delete task
+function deleteTask(element){
+  const deleteButton = element.querySelector('.task__button_type_delete');
+  deleteButton.addEventListener('click', () => element.remove());
+}
+
 //Listeners delete btn click-copy dblclick-edit
 function handleEventListeners(){
   const tasksSet = document.querySelectorAll('.task');
   tasksSet.forEach(elem => {
-    const deleteButton = elem.querySelector('.task__button_type_delete');
-    deleteButton.addEventListener('click', () => elem.remove());
-    elem.removeEventListener('dblclick', editTask);
-    elem.addEventListener('dblclick', editTask);
-  })
+    deleteTask(elem);
+    const doneButton = elem.querySelector('.task__button_type_done');
 
+    doneButton.removeEventListener('click', doneTask);
+    doneButton.addEventListener('click', doneTask);
+    //Listeners for edit tasks.
+    // elem.removeEventListener('dblclick', editTask);
+    // elem.addEventListener('dblclick', editTask);
+  })
 }
 
+//Function for button 'done'. Find parent (task) and change color.
+function doneTask(){
+  const button = this;
+  // const taskElement = button.parentNode.parentNode;
+  // taskElement.classList.toggle('task_done')
+  const taskElement = button.closest('.task');
+  taskElement.classList.toggle('task_done');
+}
+
+//Function edit task. work not correct. dbl work on all task, need only on text.  need fix it.
+//If use parentNode or closest work only 1 time.
 function editTask(){
   const elem = this;
+  // const elem = text.parentNode;
   if(!elem.classList.contains('edit')){
     elem.classList.add('edit');
     const text = elem.querySelector('.task__title');
     const input = document.createElement('input');
     input.classList.add('task__input');
-    elem.prepend(input)
+    elem.prepend(input);
     input.value = text.textContent;
     text.remove();
     input.addEventListener('blur', () => {
       const text = document.createElement('h2');
       text.classList.add('task__title');
       text.textContent = input.value;
-      input.remove()
+      input.remove();
       elem.prepend(text);
       elem.classList.remove('edit');
-      inputForm.value = '';
+      clearInput(inputForm);
     })
   }
 }
 
 buttonConfirm.addEventListener('click', (evt) => {
-  addTaskToPage(evt);
-  handleEventListeners()
+  addTask(evt, daysWeek);
+  handleEventListeners();
 });
-
-
